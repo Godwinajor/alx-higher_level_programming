06995afb9f2b929c69bb794fc8f7ff8efdeefa20
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-11-29 22:24:19 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3 
- number = 97 
- while number < 123: 
-     print("{:s}".format(chr(number)), end='') 
-     number += 1
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
