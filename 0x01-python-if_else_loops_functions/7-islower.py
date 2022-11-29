9d7ad50f14a639b0e82caf04555b0df62a58fafe
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-11-29 22:34:10 +0100

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3 
- def islower(c): 
-     if ord(c) >= 97 and ord(c) <= 122: 
-         return 1 
-     else: 
-         return 0
+#!/usr/bin/python3
+# Author -Godwinajor
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
