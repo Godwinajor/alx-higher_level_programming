3ab40a3c4e1802e286b2470e9467cf4451b1da6c
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-11-29 22:35:15 +0100

    Update 8-upercase.py

--- 0x01-python-if_else_loops_functions/8-upercase.py
+++ 0x01-python-if_else_loops_functions/8-upercase.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3 
- def islower(c): 
-     if ord(c) >= 97 and ord(c) <= 122: 
-         return 1 
-     else: 
-         return 0
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
