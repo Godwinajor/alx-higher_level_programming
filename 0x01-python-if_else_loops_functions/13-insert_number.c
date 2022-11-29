57cec96c8c3b2a90d3ce2719f386e3fc6091b1fc
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-11-29 22:22:59 +0100

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,34 @@
-#include <stdlib.h> 
- #include <string.h> 
- #include <stdio.h> 
- #include "lists.h" 
- /** 
-  * insert_node - inserts a number in an ordered linked list 
-  * @head: double pointer to the linked list 
-  * @number: number to insert in the new node 
-  * 
-  * Return: address of the new node, or NULL 
-  */ 
- listint_t *insert_node(listint_t **head, int number) 
- { 
-         listint_t *new, *tmp_head, *prev_node; 
-  
-         tmp_head = *head; 
-         new = malloc(sizeof(listint_t)); 
-         if (!new) 
-                 return (NULL); 
-         new->n = number; 
-         if (!*head || (*head)->n > number) 
-         { 
-                 new->next = *head; 
-                 *head = new; 
-                 return (*head); 
-         } 
-         while (tmp_head) 
-         { 
-                 if (tmp_head->n > number) 
-                 { 
-                         break; 
-                 } 
-                 prev_node = tmp_head; 
-                 tmp_head = tmp_head->next; 
-         } 
-         new->next = tmp_head; 
-         prev_node->next = new; 
-         return (*head); 
- }
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ * Author -Godwinajor
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
