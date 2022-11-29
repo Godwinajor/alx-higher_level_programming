d7957672c0f6ec4610311993baeaac221e7afb72
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-11-29 22:25:27 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python3 
- number = 97 
- while(number < 123): 
-     if 113 == number or 101 == number: 
-         number += 1 
-         continue 
-     print("{:s}".format(chr(number)), end='') 
-     number += 1
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
