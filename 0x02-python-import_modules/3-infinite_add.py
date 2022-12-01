fee2db295f2ec7a875d86a72cfcb5d95f4a100d8
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-12-01 18:23:56 +0100

    Update 3-infinite_add.py
    Python is cool

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    result = 0
     import sys
+    sum = 0
     for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
