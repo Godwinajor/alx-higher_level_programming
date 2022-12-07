9a6feb6c352409e60d0cfd51aba313389b850d13
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-12-07 21:03:03 +0100

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 
-def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return result
+def common_elements(set_1, set_2):
+    return (set_1 & set_2)
