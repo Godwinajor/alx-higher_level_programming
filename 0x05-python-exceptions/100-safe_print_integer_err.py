460d8da641dbceaa4bc82ff012aff93c6b8e2a61
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-12-19 12:26:49 +0100

    Update 100-safe_print_integer_err.py
    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python3 
- def safe_print_integer_err(value): 
-     import sys 
-     try: 
-         print("{:d}".format(value)) 
-         return True 
-     except Exception as value: 
-         print("Exception: {}". format(value), file=sys.stderr) 
-         return False
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    check = True
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        check = False
+    finally:
+        return (check)
