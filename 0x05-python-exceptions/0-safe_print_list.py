26b9ddb9bb991df496f31f5a69530337a5b2d8e1
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-12-19 12:11:30 +0100

    Update 0-safe_print_list.py
    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3 
- def safe_print_list(my_list=[], x=0): 
-     count = 0 
-     for i in range(0, x): 
-         try: 
-             print("{}".format(my_list[i]), end='') 
-             count += 1 
-         except: 
-             continue 
-     print() 
-     return count
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    index = printed_idx = 0
+    while True:
+        try:
+            if index < x:
+                print("{:d}".format(my_list[index]), end='')
+                index += 1
+                printed_idx += 1
+            else:
+                print()
+                return (printed_idx)
+        except Exception:
+            index += 1
