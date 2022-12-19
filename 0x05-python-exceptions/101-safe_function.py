d00dda3534ddcec8c7acad28ae429b6ef8034325
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2022-12-19 12:28:54 +0100

    Update 101-safe_function.py
    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,12 @@
-#!/usr/bin/python3 
- def safe_function(fct, *args): 
-     import sys 
-     try: 
-         return fct(*args) 
-     except Exception as value: 
-         print("Exception: {}".format(value), file=sys.stderr) 
-         return None
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except (ZeroDivisionError, ValueError, TypeError, IndexError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        result = None
+    finally:
+        return (result)
