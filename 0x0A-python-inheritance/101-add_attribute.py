80fcf99e6197694dfa0382595fc011f9c189c7db
GodwinAjor <Godwinajor>
2023-01-09 12:26:24 -0800

    Tough days ahead of me

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""this module defines a class MyInt that inherits from int"""
+"""this module defines a function that adds attributes to objects"""
 
 
-class MyInt(int):
-    """Invert int operators == and !="""
-
-    def __eq__(self, value):
-        """Override == opeartor with != behavior"""
-        return self.real != value
-
-    def __ne__(self, value):
-        """Override != operator with == behavior"""
-        return self.real == value
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
