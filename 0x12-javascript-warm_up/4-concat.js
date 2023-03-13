b0d6a94cec4af9bc499be8278ad5354666dc98c0
GodwinAjor <Godwinajor>
2023-03-13 08:35:21 -0700

    JavaScript is Amazing

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
-const firstArg = process.argv[2];
-const secondArg = process.argv[3];
-console.log(firstArg + ' is ' + secondArg
+#!/usr/bin/node 
+  
+ console.log(`${process.argv[2]} is ${process.argv[3]}`);
